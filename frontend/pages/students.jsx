8d087b109849e970e1851c47a691815ccaa61c2a
Evans007 <ebounce500@gmail.com>
2023-06-25 23:06:19 +0100

    Frontend students page styling

--- frontend/pages/students.jsx
+++ frontend/pages/students.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Students = () => {
   return (
@@ -9,18 +10,24 @@ const Students = () => {
       </div>
       
       <div className="flex flex-row mt-20 flex-wrap justify-center gap-16 items-center">
-        <div className="w-[20rem] h-60 rounded-lg my-20 bg-slate-200">
+        <div className="w-[20rem] h-60 cursor-pointer rounded-lg my-20 bg-slate-200">
 
         </div>
-        <div className="w-[20rem] h-60 rounded-lg bg-slate-200">
+        <div className="w-[20rem] h-60 rounded-lg bg-slate-200 cursor-pointer">
+            <span>Institute</span> <br /> <span>Attendance rate</span>
+        </div>
+        <div className="w-[20rem] h-60 rounded-lg cursor-pointer bg-slate-200">
 
         </div>
-        <div className="w-[20rem] h-60 rounded-lg bg-slate-200">
+        <div className="w-[20rem] rounded-lg cursor-pointer h-60 bg-slate-200">
 
         </div>
-        <div className="w-[20rem] rounded-lg h-60 bg-slate-200">
+        <div className="w-[20rem] rounded-lg cursor-pointer h-60 bg-slate-200">
 
         </div>
+      </div>
+      <div>
+        < Footer />
       </div>
     </div>
   );
