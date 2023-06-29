adca60dc85ce95e35749fb902546d300bcfb7660
olorunfemibabs <olorunfemisamuel01@gmail.com>
2023-06-29 13:55:50 +0100

    programme page

---
+++ frontend/pages/programmes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Programmes from "../src/Pages/Programmes";
+
+const programmes = () => {
+  return (
+    <div>
+      <Programmes />
+    </div>
+  );
+};
+
+export default programmes;
