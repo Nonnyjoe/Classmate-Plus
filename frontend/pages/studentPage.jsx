f1eb8f5dd1b5480ddc357133947951b480a99617
Evans007 <ebounce500@gmail.com>
2023-06-29 02:20:15 +0100

    Integrating studentsPage

--- frontend/pages/studentPage.jsx
+++ frontend/pages/studentPage.jsx
@@ -4,10 +4,43 @@ import Footer from "../components/Footer";
 import ChildABI from '../utils/childABI.json';
 import contractAddress from '../utils/contractAddr';
 import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
+import { toast } from "react-toastify";
 
 
 const StudentPage = () => {
   const [ id, setId ] = useState(0);
+
+  const { config: config1 } = usePrepareContractWrite({
+    address: contractAddress,
+    abi: ChildABI,
+    functionName: 'signAttendance',
+    args: [
+      id
+    ],
+  });
+
+  const {
+    data: signAttendanceData,
+    isLoading: signAttendanceIsLoading,
+    write: sign,
+  } = useContractWrite(config1);
+
+  const {
+    data: signAttendanceWaitData,
+    isLoading: signAttendanceWaitDataIsLoading,
+    isError,
+    isSuccess,
+  } = useWaitForTransaction({
+    hash: signAttendanceData?.hash,
+
+    onSuccess: () => {
+      toast.success('Attendance signed successfully');
+    },
+
+    onError(error) {
+      toast.error('Sign attendance error: ', error);
+    },
+  })
 
 
   return (
