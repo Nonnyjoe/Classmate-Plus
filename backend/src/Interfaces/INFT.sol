db1c58065e4c8e7c07f3f1fb87dec496b336932a
idogwu chinonso <idogwuchinonso1@gmail.com>
2023-07-03 23:02:10 +0100

    contract: updated uint based ID to bytes

--- backend/src/Interfaces/INFT.sol
+++ backend/src/Interfaces/INFT.sol
@@ -2,7 +2,11 @@
 pragma solidity ^0.8.13;
 
 interface INFT {
-    function mint(address _to, uint256 _daysId, uint256 _amount) external;
+    function mint(
+        address _to,
+        bytes calldata _daysId,
+        uint256 _amount
+    ) external;
 
-    function setDayUri(uint256 id, string memory _uri) external;
+    function setDayUri(bytes calldata id, string memory _uri) external;
 }
