2110a4428136cb235e5ceae6a3eab8a98bd6a311
Nonnyjoe <idogwuchinonso1@gmail.com>
2023-10-10 08:34:07 +0100

    demo

--- frontend/utils/contractAddress.js
+++ frontend/utils/contractAddress.js
@@ -1,5 +1,5 @@
 export const FacoryAddr = () => {
-  return "0x2dFA8fA8D6Ba183c05F715a9b24E71188C569038";
+  return "0xeee541e22691718eD9bea0aCD22B43a6d578dCFb";
 };
 export const ChildAddr = () => {
   return "0x218A031C2EB12ABcA17FdB5fC72620dA9CaDeF9B";
